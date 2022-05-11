c516c157a764421c76899d216c786d08a1ef7b78
Nithin Chintala <chintala.v@northeastern.edu>
2022-05-11 12:16:47 -0400

    initial commit; core parser built

---
+++ README.md
@@ -0,0 +1,17 @@
+# molextract
+
+A rule-based parser for Molcas/OpenMolcas log files to extract and transform any data
+found in a log file.
+
+Molextract provides a more modular approach to extracting information from log files and
+abstracts out similar control flow seen in many scripts.
+
+Molextract is abstract enough that it really isn't specific to Molcas/OpenMolcas log files.
+Any file that has certain start / end markers can use the same infrastructure.
+
+## High Level Overview
+
+## Installation
+
+## Examples
+
---
+++ rules/rasscf.py
@@ -0,0 +1,34 @@
+import molextract as me
+
+class RASSCFEnergy(me.Rule):
+
+    TRIGGER = r"::    RASSCF root number"
+    END = r"^\s+$"
+
+    def __init__(self):
+        super().__init__(self.TRIGGER, self.END)
+        self.state = []
+
+    def energy(self, line):
+        return float(line.split()[7])
+
+    def feed(self, line):
+        self.state.append(self.energy(line))
+        for line in self:
+            self.state.append(self.energy(line))
+
+    def clear(self):
+        copy = self.state.copy()
+        self.state.clear()
+        return copy
+
+
+me.DEBUG = True
+parser = me.MolcasParser(RASSCFEnergy())
+parser.cli()
+
+# with open("FMNhq_Ph-2.log", "r") as log:
+#     data = log.read()
+#
+# parsed = parser.feed(data)
+# print(json.dumps(parsed, indent=4))
