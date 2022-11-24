d96ad39faf8100b00889df14a5ca0f35916a008b
Nithin Chintala <>
2022-11-24 17:23:39 -0500

    Rule class docstring

--- molextract/rule.py
+++ molextract/rule.py
@@ -3,6 +3,54 @@ import re
 
 class Rule:
     """
+    A Rule is the core mechanism used to parse data. It is based on the assumption
+    that the data you want to parse has relevant information is between hallmark
+    start and end tag. For example take the text file below
+
+        BEGIN DATA
+        1.4 3 2
+        3.6 5 7
+        8.9 7 9
+        END DATA
+
+    Here it is clear that the data lays between "BEGIN DATA" and "END DATA". A
+    Rule explicitly defines these rules via RegularExpression. So describe the
+    above data we can have a rule as follows:
+
+        class TestRule:
+            START_TAG = "BEGIN DATA"
+            END_TAG = "END DATA"
+
+    To set the the data this rule should parse we call the `set_iter` method:
+
+        rule = TestRule()
+        rule.set_iter(<iterator>)
+
+    To actually gain access to this data we define a method process_lines:
+
+        def __init__(self):
+            super().__init__
+            self.data = []
+
+        def process_lines(self, start_line):
+            # assert start_line == "BEGIN DATA"
+            for line in self:
+                nums = [float(num) for num in line.split()]
+                self.data.append(nums)
+
+    The rule will only iterate though the iterator until it find a string that
+    matches the defined END_TAG
+
+    To finally get access to the parsed data you should define the `reset` method
+    to reset any internal state and return the parsed data. The goal of this method is
+    to allow the re-use of instances of Rule.
+
+        def reset(self):
+            tmp = self.data.copy()
+            self.data.clear()
+            return tmp
+
+
     :cvar START_TAG: The regular expression that defines when to begin parsing
     :cvar END_TAG: the regular expression that defines when to stop parsing
     :cvar CHECK_ONLY_BEGINNING: whether the START_TAG and END_TAG should only
@@ -125,7 +173,3 @@ class Rule:
             raise StopIteration
 
         return line
-
-
-class SingleLineRule(Rule):
-    pass
