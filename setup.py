31e6ef02393ac4725f8aed4a111841f7e228a3d5
Nithin Chintala <>
2022-11-26 15:03:42 -0500

    added setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setup
+
+setup(
+    name='molextract',
+    version='0.5.0',    
+    description='Parse Molcas/OpenMolcas Log Files',
+    url='https://github.com/sdonglab/molextract',
+    author='Nithin Chintala',
+    author_email='chintala.v@northeastern.edu',
+    license='MIT',
+    packages=['molextract'],
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Chemistry',
+    ],
+)
