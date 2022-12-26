dc3857e8c58c86b74ec04cf4334bb79a84e195f8
Nithin Chintala <>
2022-12-26 18:08:58 -0500

    move log dir to test_files dir

---
+++ molextract/util.py
@@ -0,0 +1,12 @@
+import molextract
+import pathlib
+
+MOLEXTRACT_MODULE_DIR = pathlib.Path(molextract.__file__).parent
+TEST_FILE_DIR = MOLEXTRACT_MODULE_DIR / 'test_files'
+
+def molextract_test_file(name) -> pathlib.Path:
+    path = TEST_FILE_DIR / name
+    assert path.exists(), f"test file '{name}' does not exist"
+    return path
+
+
