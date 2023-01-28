ce5a20985d28a5a6b743e20564e34fe3ec992357
Nithin Chintala <chintala.v@northeastern.edu>
2023-01-28 11:50:33 -0500

    trying to get pip to work

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='molextract',
-    version='0.5.0',
-    description='Parse Molcas/OpenMolcas Log Files',
-    url='https://github.com/sdonglab/molextract',
-    author='Nithin Chintala',
-    author_email='chintala.v@northeastern.edu',
-    license='MIT',
-    packages=['molextract'],
-    classifiers=[
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Scientific/Engineering :: Chemistry',
-    ],
-)
+setup()
